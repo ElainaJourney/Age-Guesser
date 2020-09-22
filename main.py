bf31b8cb290c450c0cc1b83ca8a0de0d33807e82
Samuel Rowan <samuel@samuelrowan.com>
2020-09-22 18:42:06 +0000

    Added the ability to guess year of birth

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from datetime import datetime
 
-birth_year = int(input("what year were you born?"))
 current_year = int(datetime.now().year)
-age = current_year - birth_year
+current_month = int(datetime.now().month)
+year_or_age = input("Do you know the year or age?:  ")
 
-print(f'you are either {age -1} or {age} years old')
+if (year_or_age == "year" or year_or_age == "Year"):
+    birth_year = int(input("What year were you born?:  "))
+    age = current_year - birth_year
+    print(f'You are either {age -1} or {age} years old')
+if year_or_age == "age" or year_or_age == "Age":
+    age = int(input("How old are you?:  "))
+    birth_year = current_year - age
+    print(f'You were born in {birth_year} or {birth_year + 1}.')
+else:
+    print("Try again")
